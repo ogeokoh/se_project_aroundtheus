7285a0f7570960eed5a68eb6e45cda768ea58f31
Oge Okoh <ogechukwu.okoh@gmail.com>
2024-08-24 12:45:32 -0500

    Added functionality to Edit profile and close modal

--- scripts/index.js
+++ scripts/index.js
@@ -30,4 +30,18 @@ const initialCards = [
   },
 ];
 
-console.log(initialCards);
+/* Elements */
+
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileEditCloseButton = document.querySelector(
+  "#profile-edit-close-button"
+);
+
+profileEditButton.addEventListener("click", () => {
+  profileEditModal.classList.add("modal__opened");
+});
+
+profileEditCloseButton.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal__opened");
+});
