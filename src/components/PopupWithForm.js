0d66167aec01721a6ec0ef5c916500084e2e1c4b
Oge Okoh <ogechukwu.okoh@gmail.com>
2025-11-12 22:58:52 -0600

    project 9 submission.

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -9,6 +9,27 @@ export default class PopupWithForm extends Popup {
     this._inputs = Array.from(
       this._popupElement.querySelectorAll(".modal__input")
     );
+
+    // NEW: submit button + its default label
+    this._submitButton = this._popupElement.querySelector(".modal__button");
+    this._defaultSubmitText = this._submitButton
+      ? this._submitButton.textContent
+      : "";
+  }
+
+  // NEW: toggle loading UI (text + disable inputs/button)
+  renderLoading(isLoading, loadingText = "Saving...") {
+    if (!this._submitButton) return;
+
+    if (isLoading) {
+      this._submitButton.textContent = loadingText;
+      this._submitButton.disabled = true;
+      this._inputs.forEach((i) => (i.disabled = true));
+    } else {
+      this._submitButton.textContent = this._defaultSubmitText;
+      this._submitButton.disabled = false;
+      this._inputs.forEach((i) => (i.disabled = false));
+    }
   }
 
   _getInputValues() {
@@ -30,5 +51,7 @@ export default class PopupWithForm extends Popup {
   close() {
     super.close();
     this._form.reset();
+    // NEW: always restore default UI if popup is closed mid-request
+    this.renderLoading(false);
   }
 }
